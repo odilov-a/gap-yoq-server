90f943eb3d72983d884521378b63c8266c8228b1
odilov-a <akbarjonodilov01@gmail.com>
2024-06-05 22:32:24 +0500

    chore: Remove commented code and optimize image upload in upload middleware

--- models/Gallery.js
+++ models/Gallery.js
@@ -1,24 +1,14 @@
 const mongoose = require("mongoose");
-const TYPE_UZ = ["animal", "oil", "cotton", "village", "spinning", "weaving", "paint", "sewing"];
-const TYPE_RU = ["animal", "oil", "cotton", "village", "spinning", "weaving", "paint", "sewing"];
-const TYPE_EN = ["animal", "oil", "cotton", "village", "spinning", "weaving", "paint", "sewing"];
+const TYPES = ["animal", "oil", "cotton", "village", "spinning", "weaving", "paint", "sewing"];
 const GallerySchema = new mongoose.Schema(
   {
     image: {
       type: Array,
       required : true,
     },
-    typeUz: {
+    type: {
       type: String,
-      enum: TYPE_UZ,
-    },
-    typeRu: {
-      type: String,
-      enum: TYPE_RU,
-    },
-    typeEn: {
-      type: String,
-      enum: TYPE_EN,
+      enum: TYPES,
     },
   },
   { timestamps: true }
